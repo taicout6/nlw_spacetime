3e3298c727ae3b30219cddab9ed15d939e710e6b
Tainá Coutinho <taicout6@gmail.com>
2023-05-16 16:45:33 -0300

    Add front-end com next.js

---
+++ web/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/Button'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello World</h1>
+    
+      <Button title='Tai' />
+      <Button title='S2' />
+      <Button title='Bele' />
+    </>
+  )
+}
