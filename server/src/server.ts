40f0e8de45f4c7f451774a7dcb7bc1c4aa3c2cf0
Tainá Coutinho <tai@pop-os.localdomain>
2023-05-16 14:58:49 -0300

    Add eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello World'
 })
 
-app.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+  })
